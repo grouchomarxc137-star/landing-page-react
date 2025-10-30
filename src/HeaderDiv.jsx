eb98e3aefea5aac1177a3bdaf013dd22497e947f
grouchomarxc137-star <grouchomarxc137@gmail.com>
2025-10-30 22:35:44 +0100

    Added the header

---
+++ src/HeaderDiv.jsx
@@ -0,0 +1,13 @@
+import "./headerDiv.css"
+
+function HeaderDiv(){
+    return(
+    <div className="header-div">
+            <p className="hp1">Header Logo</p>
+            <p className="hp2">header link one</p>
+            <p className="hp3">header link two</p>
+            <p className="hp4">header link three</p>
+    </div>
+    )
+}
+export default HeaderDiv;
