1251228bcb1f7ca52eefb4ab5942646a974dda94
grouchomarxc137-star <grouchomarxc137@gmail.com>
2025-10-30 22:44:37 +0100

    Added Main div section 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import HeaderDiv from './HeaderDiv'
+import HeaderDiv from './header/HeaderDiv.jsx'
+import MainDivSection1 from './main_div/MainDivSection1.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HeaderDiv />
+    <MainDivSection1 />
   </StrictMode>,
 )
---
+++ src/main_div/MainDivSection1.jsx
@@ -0,0 +1,20 @@
+import "./mainDivSection1.css"
+
+function MainDivSection1(){
+    return(
+        <div className="m1-div">
+                <div className="m1-div-container1">
+                    <h1>This website is <br />awesome</h1>
+                    <p>This website has some subtext that goes here under
+                        the<br /> main title. It's a smaller font and the color is lower<br />
+                        contrast
+                    </p>
+                    <button>Sign Up</button>
+                </div>
+                <div className="m1-div-container2">
+                    <div className="img-plc">This is a placeholder for an image</div>
+                </div>
+        </div>
+    )
+}
+export default MainDivSection1;
