4228703e53e1522cc355bcdefeb10c6999c86c44
grouchomarxc137-star <grouchomarxc137@gmail.com>
2025-10-30 22:51:23 +0100

    Added Main div section 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import HeaderDiv from './header/HeaderDiv.jsx'
 import MainDivSection1 from './main_div/MainDivSection1.jsx'
+import MainDivSection2 from './main_div/MainDivSection2.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HeaderDiv />
     <MainDivSection1 />
+    <MainDivSection2 />
   </StrictMode>,
 )
---
+++ src/main_div/MainDivSection2.jsx
@@ -0,0 +1,24 @@
+import "./mainDivSection2.css"
+
+function MainDivSection2() {
+    return (
+        <div className="m2-div">
+                <div className="m2-div-container1">
+                    <h1>Some random information.</h1>
+                </div>
+                <div className="m2-div-container2">
+                    <div className="info-box"></div>
+                    <div className="info-box"></div>
+                    <div className="info-box"></div>
+                    <div className="info-box"></div>
+                </div>
+                <div className="m2-div-container3">
+                    <div className="info-text">This is some subtext<br />under an illustration<br />or image</div>
+                    <div className="info-text">This is some subtext<br />under an illustration<br />or image</div>
+                    <div className="info-text">This is some subtext<br />under an illustration<br />or image</div>
+                    <div className="info-text">This is some subtext<br />under an illustration<br />or image</div>
+                </div>
+            </div>
+    )
+}
+export default MainDivSection2
